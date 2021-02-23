9d8db9874481f67f5b47e344d2b6bbfdfb29f0eb
Resakoso <dam07.2021@gmail.com>
2021-02-23 19:44:29 +0100

    initial commit

---
+++ src/ed/examen/modelo/CursoTest.java
@@ -0,0 +1,34 @@
+package ed.examen.modelo;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+class CursoTest {
+
+	@Test
+	void testEliminarAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAniadirAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaRegistrado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNumeroAlumnos() {
+		fail("Not yet implemented");
+	}
+
+}
